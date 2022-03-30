fdd515fdb5c34c695f4a255eeedf9ab6125d3bf5
Pajehome <abdel20069@gmail.com>
2022-03-30 18:53:59 +0200

    active

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-myImage = document.getElementById("mainImg");
+const myImage = document.getElementById("mainImg");
 const imgArr = [
     'img/01.jpg',
     'img/02.jpg',
@@ -6,6 +6,7 @@ const imgArr = [
     'img/04.jpg',
     'img/05.jpg'
 ]
+
 let imgIndex = 1;
 
 function nextImg() {
@@ -23,3 +24,17 @@ function prevImg() {
         imgIndex = imgArr.length -1 ;
     }
 }
+
+let listImg = document.querySelectorAll('.rightImg .vid');
+
+listImg.forEach(img =>{
+ img.onclick = () =>{
+    listImg.forEach(vid => vid.classList.remove('active'))
+    img.classList.add('active')
+
+    if(img.classList.contains('active')){
+        let src = img.getAttribute('src');
+        myImage.src = src;
+    }
+ }
+});
